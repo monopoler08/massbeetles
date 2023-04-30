903e01cba1536abc9a9673115ab2a3e82303f3e5
Chris Forman Orth <cformanorth@gmail.com>
2023-04-30 14:30:01 -0400

    got rid of weird file

--- queries.py
+++ queries.py
@@ -1,62 +1,97 @@
-from sqlalchemy import select, func
-from models.base import Session
-from author import Author
-from county import County
-from family import Family
-from genus import Genus
-from publication import Publication, PublicationsSpecies
-from record import Record
-from source import Source
-from species import Species
-from state import State
-from super_family import SuperFamily
-from sub_order import SubOrder
-from synonym import Synonym
-from common_name import CommonName
+from sqlalchemy import select, func, create_engine
+from sqlalchemy.orm import Session
+from models.author import Author
+from models.county import County
+from models.family import Family
+from models.genus import Genus
+from models.publication import Publication
+from models.publications_species import PublicationsSpecies
+from models.record import Record
+from models.source import Source
+from models.species import Species
+from models.state import State
+from models.super_family import SuperFamily
+from models.sub_order import SubOrder
+from models.synonym import Synonym
+from models.common_name import CommonName, common_names_species_association
 import pandas as pd
-from sqlalchemy.orm import configure_mappers
+import json
 
-session = Session()
-configure_mappers()
+engine = create_engine("sqlite:///beetles.db")
 
-df = pd.read_csv("beetles.csv")
+csv = pd.read_csv("beetles.csv")
 
-csv_species_list = set(df["Scientific Name"].values.tolist())
+for row in csv[["Species", "Genus"]].drop_duplicates().to_dict("records"):
+    with Session(engine) as session:
+        species = (
+            session.query(Species)
+            .join(Species.genus)
+            .where(Species.name == row["Species"])
+            .where(Genus.name == row["Genus"])
+        ).all()
+        if not species:
+            print(row["Genus"], row["Species"])
 
-db_species = (
-    session.query(Species)
-    .join(Species.genus)
-    .join(Species.records)
-    .outerjoin(Species.synonyms)
-    .outerjoin(Species.common_names)
-    .outerjoin(Record.county)
-    .outerjoin(Record.source)
-    .join(Genus.family)
-    .outerjoin(Species.publications_species)
-    .outerjoin(PublicationsSpecies.publication)
-    .outerjoin(Publication.author)
-    .group_by(Species)
-)
+with Session(engine) as session:
+    all_species = {x.scientific_name() for x in session.query(Species).all()}
 
-db_species_list = set([species.scientific_name() for species in db_species])
+    species_records = (
+        session.query(Record)
+        .join(Record.species)
+        .join(Species.genus)
+        .join(Species.common_names, isouter=True)
+        .join(Species.synonyms, isouter=True)
+        .join(Genus.family)
+        .join(Record.source)
+        .join(Record.county, isouter=True)
+        .order_by(Genus.name, Species.name)
+    )
 
-difflist = list(csv_species_list - db_species_list)
+    species_data = dict()
+    for r in species_records:
+        species = species_data.get(
+            r.species.scientific_name(),
+            {
+                "name": r.species.scientific_name(),
+                "genus": r.species.genus.name,
+                "family": r.species.genus.family.name,
+                "common_names": [cn.name for cn in r.species.common_names],
+                "synonyms": [sn.name for sn in r.species.synonyms],
+                "county_records": dict(),
+            },
+        )
+        if not r.county or r.county.name == "null":
+            county_name = "Statewide"
+        else:
+            county_name = r.county.name
 
-difflist.sort()
-print(difflist)
-print(df[["Family", "Scientific Name"]][df["Scientific Name"].isin(difflist)])
+        county_records = species["county_records"].get(county_name, [])
+        county_records.append(
+            {
+                "name": r.source.name,
+                "person": r.source.person,
+                "year": r.source.year,
+                "url": r.source.url,
+                "journal": r.source.journal,
+                "volume": r.source.volume,
+            }
+        )
+        species["county_records"][county_name] = county_records
+        species_data[r.species.scientific_name()] = species
 
-gcount = { count_tuple[0]:count_tuple[1] for count_tuple in session.query(Genus.name,func.count(Species.genus)).join(Species.genus).group_by(Genus.name).all() }
+    for s, d in species_data.items():
+        if len(d["county_records"].keys()) > 1:
+            print(s, d["county_records"])
 
-gcount_sp_only = { count_tuple[0]:count_tuple[1] for count_tuple in session.query(Genus.name,func.count(Species.genus)).join(Species.genus).filter(Species.name.in_(['sp.'])).group_by(Genus.name).all() }
+        #    for i in all_species:
+        #        if i not in species_data.keys():
+        #            print(i)
 
-total = 0
-for genus in gcount.keys():
-    if gcount[genus] == gcount_sp_only.get(genus,0):
-        gcount[genus] = 1
-    else:
-        gcount[genus] = gcount[genus] - gcount_sp_only.get(genus,0)
-    total = total + gcount[genus]
-    print(f'{genus},{gcount[genus]}')
+#    mapped_species = {i for i in species_data.keys()}
 
-print(total)
+#    print(len(mapped_species))
+#    print(len(all_species))
+
+#    for i in all_species:
+#        if i not in mapped_species:
+#            print(i)
